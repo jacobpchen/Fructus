0860c00c00c644840206ed66b748a2da596da1fe
Jacob Chen <jacobpchen@gmail.com>
2021-05-09 08:53:10 -0400

    upload initial files

---
+++ Fructus/View/StartButtonView.swift
@@ -0,0 +1,26 @@
+//
+//  StartButtonView.swift
+//  Fructus
+//
+//  Created by Jacob Chen on 5/9/21.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    // MARK: Properties
+    
+    // MARK: Button
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+// MARL: Preview
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+            .previewLayout(.sizeThatFits)
+    }
+}
